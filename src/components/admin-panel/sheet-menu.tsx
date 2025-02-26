f0992948548f7d7a66aaa223b1743a5bb50e9b54
danyoh <danyoh@gmail.com>
2025-02-26 13:34:40 +0900

    1

--- src/components/admin-panel/sheet-menu.tsx
+++ src/components/admin-panel/sheet-menu.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import { MenuIcon, PanelsTopLeft } from "lucide-react";
+import { MenuIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Menu } from "@/components/admin-panel/menu";
@@ -9,7 +8,7 @@ import {
   SheetContent,
   SheetTrigger,
   SheetTitle,
-  SheetDescription
+  SheetDescription,
 } from "@/components/ui/sheet";
 import Image from "next/image";
 
@@ -24,19 +23,24 @@ export function SheetMenu() {
       <SheetTitle className="sr-only">Popup Menu</SheetTitle>
       <SheetContent className="sm:w-72 px-3 h-full flex flex-col" side="left">
         <SheetHeader>
-        <SheetDescription className="sr-only">
-        사이즈가 적을때 나타나는 팝업 메뉴
-      </SheetDescription>
-          <div className="flex items-center justify-center gap-2 " style={{ height: 'var(--navbar-height)' }}>
+          <SheetDescription className="sr-only">
+            사이즈가 적을때 나타나는 팝업 메뉴
+          </SheetDescription>
+          <div
+            className="flex items-center justify-center gap-2 "
+            style={{ height: "var(--navbar-height)" }}
+          >
             <Image
               src="/image/myimage/dalcomlab-log.avif"
               alt=""
               width={42}
               height={42}
-              style={{ width: '42px', height: '42px' }}
+              style={{ width: "42px", height: "42px" }}
             />
             <div className="flex flex-col">
-              <span className="text-xs leading-3 font-medium">Dalcomlab Ai</span>
+              <span className="text-xs leading-3 font-medium">
+                Dalcomlab Ai
+              </span>
               <span className="text-[10px] text-gray-500">Email analyze</span>
             </div>
           </div>
